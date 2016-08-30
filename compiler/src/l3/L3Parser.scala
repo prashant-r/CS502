b253eeadb9a7113d5ef5728fcc39319a9fc59c43
Prashant Ravi <prashant.ravi@gmail.com>
2016-08-29 20:08:29 -0400

    project 1 completed

--- compiler/src/l3/L3Parser.scala
+++ compiler/src/l3/L3Parser.scala
@@ -174,25 +174,65 @@ object L3Parser {
 
   // Syntactic sugar translation.
   private def sFun(args: Seq[String], body: Tree)
-                  (implicit p: Position): Tree = 
-    ???
+                  (implicit p: Position): Tree = {
+     val frsh = freshName("fun")
+     LetRec(Seq(FunDef(frsh, args, body)), Ident(frsh))
+  }
   private def sLet_*(bdgs: Seq[(String,Tree)], body: Tree)
                     (implicit p: Position): Tree =
-    ???
+ {
+   (bdgs :\ body) ( (b, t) => Let(List(b), t))
+ }
   private def sBegin(exprs: Seq[Tree])(implicit p: Position): Tree =
-    ???
+  {
+    exprs match {
+      case Seq(d) => d;
+      case Seq(d, ds @ _*) => (Let(Seq((freshName("begin"), d)), sBegin(ds)))  
+    }
+  }
   private def sRec(name: String, bdgs: Seq[(String, Tree)], body: Tree)
                   (implicit p: Position) =
-    ???
-  private def sAnd(es: Seq[Tree])(implicit p: Position): Tree =
-    ???
+ {
+     LetRec(Seq(FunDef(name, bdgs map { _._1 }, body)), App(Ident(name), bdgs map { _._2 }))   
+ }
+   
+  private def sAnd(es: Seq[Tree])(implicit p: Position): Tree = es match {
+    case Seq(d) => d;
+    case Seq(d, ds @ _*) => If( d, sAnd(ds), Lit(BooleanLit(false))); 
+    }
+  
   private def sOr(es: Seq[Tree])(implicit p: Position): Tree = 
-    ???
+  {
+    es match
+    {
+      case Seq(d) => d;
+      case Seq(d, ds @ _*) => 
+      {
+         val orName = freshName("or");
+         Let(Seq((orName, d)), If(Ident(orName), Ident(orName), sOr(ds))); 
+      }
+    }
+  }      
   private def sNot(e: Tree)(implicit p: Position): Tree =
-    ???
+     If( e, Lit(BooleanLit(false)), Lit(BooleanLit(true)));
   private def sCond(clses: Seq[(Tree, Seq[Tree])])(implicit p: Position): Tree =
-    ???
-
+  { 
+    clses match
+    {
+      case Seq() =>
+        Lit(UnitLit)
+      case Seq(current, continuation @ _*) => 
+      {
+        current match
+        {
+          case (s, d) =>
+          {
+              If(s, sBegin(d), sCond(continuation));    
+          }
+        }
+      }
+    }
+  }
   private def sStringLit(s: String)(implicit p: Position): Tree = {
     val b = freshName("string")
     val cs = codePoints(s)
